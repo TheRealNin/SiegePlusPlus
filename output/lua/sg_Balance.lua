f76018a69e3a32ff43145bd7245cf1c474c49170
Nin <abielins@gmail.com>
2017-07-07 10:29:22 -0400

    Update includes

--- output/lua/sg_Balance.lua
+++
@@ -1,8 +0,0 @@
-//
-//	ns2siege+ Custom Game Mode
-//	ZycaR (c) 2016
-//
-
-// increase contamination cooldown significanlty ... because siege is about turtling!
-kContaminationCooldown = 60
-kContaminationBileSpewCount = 1 
--- output/lua/sg_Server.lua
+++
@@ -1,6 +0,0 @@
-//
-//	ns2siege+ Custom Game Mode
-//	ZycaR (c) 2016
-//
-
-decoda_name = "Server"
