794a03e4830805bf862de84004589fa5c25612b4
Dalin Seivewright <dalin.seivewright@gmail.com>
2018-01-14 23:55:59 -0600

    Slight refactor of TechTree override to allow us to easily add more tech Ids to disable more tech before front doors open.  Also added the Shift Egg ability to this list to prevent them from being shifted past front door before it opens.

--- source/lua/sg_TechTree_Server.lua
+++ source/lua/sg_TechTree_Server.lua
@@ -5,10 +5,16 @@
 
 -- disable specific techs to be available before BOTH doors are opened:
 -- - 'contamination' .. as it allows exploits
+
+-- Add to list as needed to disable techs
+local techDisableList = {
+    [kTechId.Contamination] = true,
+    [kTechId.TeleportEgg] = true
+}
+
 local ns2_SetTechNodeChanged = TechTree.SetTechNodeChanged
 function TechTree:SetTechNodeChanged(node, logMsg)
-    if node:GetTechId() == kTechId.Contamination then
-
+    if techDisableList[node:GetTechId()] then
         local front, siege, suddendeath = GetGameInfoEntity():GetSiegeTimes()
         if front > 0 and siege > 0 then
             node.available = false
